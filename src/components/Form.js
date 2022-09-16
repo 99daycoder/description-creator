11e8619da470efb781779a7ff67f1d8220a5e60b
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-09-16 10:42:56 +0100

    Form component working + checks character length

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,58 @@
-import { Flex, Input, Text, Textarea } from '@chakra-ui/react'
-import React from 'react'
+import { Flex, Input, Text, Textarea } from "@chakra-ui/react";
+import React from "react";
 
+const InputField = ({ placeholder, value, onChange, charLimit }) => {
+  return (
+    <Flex direction="row">
+      <Textarea
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      ></Textarea>
+      <Text bg={value.length > charLimit ? "red" : "none"}>
+        {value.length}/{charLimit}
+      </Text>
+    </Flex>
+  );
+};
 
 export default function Form() {
+  const [valueTitle, setValueTitle] = React.useState("");
+  const [valueDescription, setValueDescription] = React.useState("");
+  const [valueKeywords, setValueKeywords] = React.useState("");
+  const handleChangeTitle = (event) => setValueTitle(event.target.value);
+  const handleChangeDescription = (event) =>
+    setValueDescription(event.target.value);
+  const handleChangeKeywords = (event) => setValueKeywords(event.target.value);
   return (
-    
-        <Flex direction='column'  margin='10px'>
-            <Flex direction='row'> 
-            <Input placeholder='Title goes here..'></Input> 
-            <Text>10/20</Text>
-            </Flex>
-
-            <Flex direction='row'> 
-            <Textarea placeholder='Description goes here'></Textarea> 
-            <Text>10/30</Text>
-            </Flex>
-        </Flex>
-    
-  )
+    <Flex direction="column" margin="10px">
+      <InputField
+        placeholder="Type your title here"
+        value={valueTitle}
+        onChange={handleChangeTitle}
+        charLimit={200}
+      />
+      <InputField
+        placeholder="Descrition goes here"
+        value={valueDescription}
+        onChange={handleChangeDescription}
+        charLimit={2000}
+      />
+      <InputField
+        placeholder="Type your backend keywords here"
+        value={valueKeywords}
+        onChange={handleChangeKeywords}
+        charLimit={250}
+      />
+    </Flex>
+  );
 }
+
+// <Flex direction="row">
+//   <Input
+//     placeholder="Title goes here.."
+//     value={value}
+//     onChange={handleChange}
+//   ></Input>
+//   <Text bg={value.length > 20 ? "red" : "none"}>{value.length}/20</Text>
+// </Flex>
